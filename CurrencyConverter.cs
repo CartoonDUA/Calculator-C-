2639942069a074cc32a933d79325e84013f2907b
CartoonDU <81403994+CartoonDUA@users.noreply.github.com>
2026-02-22 10:47:10 -0800

    Add CurrencyConverter class with exchange rates

--- CurrencyConverter.cs
+++ CurrencyConverter.cs
@@ -1,3 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+
+class CurrencyConverter
+{
+    static void Main()
+    {
+        // Exchange rates to USD
+        Dictionary<string, double> exchangeRates = new Dictionary<string, double>()
+        {
+            { "USD", 1.0 },
+            { "EUR", 0.92 },
+            { "GBP", 0.79 },
+            { "JPY", 150.25 },
+            { "CAD", 1.35 },
+            { "AUD", 1.52 }
+        };
